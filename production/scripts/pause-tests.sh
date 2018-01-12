22fffa178bd4fb6d40fdde069a90904e5a53bbf2
Chad Phillips <chad@apartmentlines.com>
2018-01-12 14:43:09 -0800

    add location support to salt master run/kill/pause helpers

--- production/scripts/pause-tests.sh
+++ production/scripts/pause-tests.sh
@@ -3,27 +3,44 @@
 # Shell wrapper to pause test runs on multiple servers.
 
 # Template name for test servers.
-SERVER_NAME_TEMPLATE="webrtc-test-[%s].stirlab.net"
+SERVER_NAME_TEMPLATE="webrtc-test-%s-[%s].stirlab.net"
 
-servers="${1}"
-test_name="${2}"
+location="${1}"
+servers="${2}"
+test_name="${3}"
 script=`basename "${0}"`
+
+locations="cs-mia pb-las"
+valid_location=0
 
 usage() {
 
   echo "
-Usage: ${script} <servers> <test_name>
+Usage: ${script} <location> <servers> <test_name>
+  location: Location label, one of: ${locations}
   servers: Single server number (1) or range (1-5)
   test_name: Name of test to pause
 "
 }
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
   usage
   exit 1
 fi
 
-server_regex=`printf ${SERVER_NAME_TEMPLATE} "${servers}"`
+for l in ${locations} do;
+  if [ "${location}" = "${l}" ]; then
+    valid_location=1
+  fi
+done
+
+if [ ${valid_location} -eq 0 ]; then
+  echo "ERROR: Invalid location"
+  usage
+  exit 1
+fi
+
+server_regex=`printf ${SERVER_NAME_TEMPLATE} "${location}" "${servers}"`
 
 salt -E "${server_regex}" cmd.run "/usr/local/bin/pause-test.sh ${test_name}"
 

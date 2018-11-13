e129783fdc427f8e8f813300c1b8440c523e9093
Chad Phillips <chad@apartmentlines.com>
2018-11-12 19:05:41 -0800

    fix up master server scripts

--- production/scripts/pause-tests.sh
+++ production/scripts/pause-tests.sh
@@ -3,44 +3,27 @@
 # Shell wrapper to pause test runs on multiple servers.
 
 # Template name for test servers.
-SERVER_NAME_TEMPLATE="webrtc-test-%s-[%s].stirlab.net"
+SERVER_NAME_TEMPLATE="webrtc-test-[%s].stirlab.net"
 
-location="${1}"
-servers="${2}"
-test_name="${3}"
+servers="${1}"
+test_name="${2}"
 script=`basename "${0}"`
-
-locations="cs-wdc"
-valid_location=0
 
 usage() {
 
   echo "
-Usage: ${script} <location> <servers> <test_name>
-  location: Location label, one of: ${locations}
+Usage: ${script} <servers> <test_name>
   servers: Single server number (1) or range (1-5)
   test_name: Name of test to pause
 "
 }
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 2 ]; then
   usage
   exit 1
 fi
 
-for l in ${locations}; do
-  if [ "${location}" = "${l}" ]; then
-    valid_location=1
-  fi
-done
-
-if [ ${valid_location} -eq 0 ]; then
-  echo "ERROR: Invalid location"
-  usage
-  exit 1
-fi
-
-server_regex=`printf ${SERVER_NAME_TEMPLATE} "${location}" "${servers}"`
+server_regex=`printf ${SERVER_NAME_TEMPLATE} "${servers}"`
 
 salt -E "${server_regex}" cmd.run "/usr/local/bin/pause-test.sh ${test_name}"
 
--- production/scripts/run-tests.sh
+++ production/scripts/run-tests.sh
@@ -3,46 +3,29 @@
 # Shell wrapper to start test runs on multiple servers.
 
 # Template name for test servers.
-SERVER_NAME_TEMPLATE="webrtc-test-%s-[%s].stirlab.net"
+SERVER_NAME_TEMPLATE="webrtc-test-[%s].stirlab.net"
 
-location="${1}"
-servers="${2}"
-test_name="${3}"
-test_data="${4}"
+servers="${1}"
+test_name="${2}"
+test_data="${3}"
 script=`basename "${0}"`
-
-locations="cs-wdc"
-valid_location=0
 
 usage() {
 
   echo "
-Usage: ${script} <location> <servers> <test_name> [data]
-  location: Location label, one of: ${locations}
+Usage: ${script} <servers> <test_name> [data]
   servers: Single server number (1) or range (1-5)
   test_name: Name of test to run
   data: Optional comma-separated data to pass to the run-test.sh script.
 "
 }
 
-if [ $# -lt 3 ] || [ $# -gt 4 ]; then
+if [ $# -lt 2 ] || [ $# -gt 3 ]; then
   usage
   exit 1
 fi
 
-for l in ${locations}; do
-  if [ "${location}" = "${l}" ]; then
-    valid_location=1
-  fi
-done
-
-if [ ${valid_location} -eq 0 ]; then
-  echo "ERROR: Invalid location"
-  usage
-  exit 1
-fi
-
-server_regex=`printf ${SERVER_NAME_TEMPLATE} "${location}" "${servers}"`
+server_regex=`printf ${SERVER_NAME_TEMPLATE} "${servers}"`
 
 salt -E "${server_regex}" cmd.run "/usr/local/bin/run-test.sh ${test_name} ${test_data}"
 
